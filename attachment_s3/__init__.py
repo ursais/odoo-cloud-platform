3f2828d812c9309ee9b045b96735b1e51fbde64b
Hiren Pattani-SerpentCS <hiren.pattani@serpentcs.com>
2021-07-09 20:05:45 +0530

    [ADD] attachment_s3: Added the post_init_hook in attachment_s3

--- attachment_s3/__init__.py
+++ attachment_s3/__init__.py
@@ -1,2 +1,9 @@
-
 from . import models
+
+from odoo import api, SUPERUSER_ID
+
+
+def _auto_create_bucket(cr, registry):
+    # create the S3 bucket after module installed
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["ir.attachment"]._get_s3_bucket()
